ef15829a039d803fadfe2a69d26141ff003b5225
Texy <anthonybarthelemy96@gmail.com>
2025-04-14 18:52:31 +0200

    Ajout des pages habitations + modifications des photos

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,9 @@ export const allRoutes = [
     new Route ("/", "Accueil", "/pages/home.html"),
     new Route ("/services", "Nos services", "/pages/services.html"),
     new Route ("/habitats", "Nos habitats et nos animaux", "/pages/habitats.html"),
+    new Route ("/savane", "La savane", "/pages/savane.html"),
+    new Route ("/jungle", "La jungle", "/pages/jungle.html"),
+    new Route ("/marais", "Le marais", "/pages/marais.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websiteName
